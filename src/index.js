f9b372711471ca5e82985b8ed4b082502158dae1
Joseph <luisj.valdiviezo20@gmail.com>
2024-03-31 21:52:29 -0500

    Change port

--- src/index.js
+++ src/index.js
@@ -201,6 +201,6 @@ app.get("*", (req, res) => {
   res.send("Not Found !");
 });
 
-app.listen(3001, () => {
-  console.log("Example app listening on port 3001!");
+app.listen(3000, () => {
+  console.log("Example app listening on port 3000!");
 });
