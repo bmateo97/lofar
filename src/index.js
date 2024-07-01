ba2111f1a75b93dba542206928eb2d032f5da4f5
bmateo97 <77997436+bmateo97@users.noreply.github.com>
2024-07-01 18:24:54 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -237,9 +237,7 @@ app.post("/email/:address", async (req, res) => {
     html:  ` <p>Gracias ${address} por su compra, esperamos que disfrute sus productos.</p>
       <p>Lista de Productos: ${productos.length}</p>
     <p> Codigo de los productos | Cantidad de productos </p>
-   
-  
-    <img src="cid:${producto.codigo}" alt="${producto.codigo}" />`).join("")}
+   ${productos.map((producto) => <p>${producto.codigo} |   ${producto.cantidad}</p>).join("")}
   
     <p>Total:$ ${total} </p>
     `,
